04605f39c0aba25e6a0b201d98665b92be25fe4c
Aleksei Khatkevich <qwerty12345@disroot.org>
2025-05-04 14:06:46 +0300

    6

--- common/resources/database/postgres/database.py
+++ common/resources/database/postgres/database.py
@@ -1,17 +1,28 @@
 from functools import cached_property
 
+import pydantic_core
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 from common import settings
 
 
 class Database:
-    def __init__(self, pg_dsn: str) -> None:
-        self.pg_dsn = pg_dsn
+    def __init__(self, **kwargs) -> None:
+        self._kwargs = kwargs
 
     @cached_property
     def engine(self) -> AsyncEngine:
-        return create_async_engine(self.pg_dsn)
+        return create_async_engine(
+            url=settings.POSTGRES_DSN.unicode_string(),
+            execution_options={},
+            insertmanyvalues_page_size=2000,
+            json_deserializer=pydantic_core.from_json,
+            json_serializer=pydantic_core.to_json,
+            echo=settings.POSTGRES_ECHO,
+            max_overflow=settings.POSTGRES_POOL_OVERFLOW,
+            pool_pre_ping=settings.POSTGRES_POOL_PRE_PING,
+            **self._kwargs,
+        )
 
 
-db = Database(settings.POSTGRES_DSN.unicode_string())
+db = Database()
--- common/settings/postgres.py
+++ common/settings/postgres.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from pydantic import Field, PostgresDsn
+from pydantic import AfterValidator, BeforeValidator, Field, PositiveInt, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -9,7 +9,36 @@ class PostgresSettings(BaseSettings):
         PostgresDsn,
         Field(repr=False, description='Урл БД Postgres',),
     ]
+
     POSTGRES_ECHO: Annotated[
         bool,
-        Field(description='Вывод SQL команд в консоль',)
+        Field(description='Вывод SQL команд в консоль',),
     ] = False
+
+    POSTGRES_ECHO_POOL: Annotated[
+        bool,
+        Field(description='Вывод информации о пуле коннектов в консоль',),
+    ] = False
+
+    POSTGRES_POOL_OVERFLOW: Annotated[
+        PositiveInt,
+        Field(description='Кол-во коннектов превышающее pool_size',),
+    ] = 10
+
+    POSTGRES_POOL_SIZE: Annotated[
+        PositiveInt,
+        Field(description='Кол-во коннектов в пуле коннектов', ),
+    ] = 5
+
+    POSTGRES_POOL_PRE_PING: Annotated[
+        bool,
+        Field(description='Проверка коннекта перед каждым чекаутом',),
+    ] = True
+
+    POSTGRES_POOL_RECYCLE: Annotated[
+        int,
+        Field(
+            AfterValidator(lambda v: max([-1, v,])),
+            description='Закрывать коннект после n секунд неактивности.',
+        ),
+    ] = -1
