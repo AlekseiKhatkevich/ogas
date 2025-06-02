c8ecfb0c0b63ad77d9349ce351a48da0d2b2a5dc
Aleksei Khatkevich <qwerty12345@disroot.org>
2025-06-02 12:15:49 +0300

    113

--- center/testing/pytests/repositories/test_organizations.py
+++ center/testing/pytests/repositories/test_organizations.py
@@ -1,16 +1,16 @@
 import typing
 
 import pytest
+import ulid
 
 if typing.TYPE_CHECKING:
-    import ulid
     from center.orm_models import OrganizationORM
     from center.repositories.postgres import OrganizationPostgresRepository
 
 
-@pytest.fixture
+@pytest.fixture(params=[{'use_id': True, 'use_name': False}, {'use_id': False, 'use_name': True}])
 def parametrize_identity(request: pytest.FixtureRequest, organization_in_db: 'OrganizationORM') -> (
-        tuple['ulid.ULID', None] | tuple[None, 'ulid.ULID']):
+        tuple[ulid.ULID, None] | tuple[None, str]):
     use_id, use_name = request.param
     return (
         organization_in_db.id if use_id else None,
@@ -18,9 +18,8 @@ def parametrize_identity(request: pytest.FixtureRequest, organization_in_db: 'Or
     )
 
 
-@pytest.mark.parametrize('parametrize_identity', [(True, False), (False, True)], indirect=True)
 async def test_get_organization_by_token_positive(
-        parametrize_identity: tuple['ulid.ULID', None] | tuple[None, 'ulid.ULID'],
+        parametrize_identity: tuple[ulid.ULID, None] | tuple[None, str],
         organization_repo: 'OrganizationPostgresRepository',
         organization_in_db: 'OrganizationORM',
         organization_token: str,
@@ -28,3 +27,18 @@ async def test_get_organization_by_token_positive(
     _id, name = parametrize_identity
     org = await organization_repo.get_organization_by_token(_id, name, organization_token)
     assert org == organization_in_db
+
+
+@pytest.mark.parametrize('use_real_id', [True, False])
+async def test_get_organization_by_token_negative_wrong_token_or_no_user(
+        use_real_id,
+        organization_repo,
+        organization_in_db,
+):
+    org = await organization_repo.get_organization_by_token(
+        organization_in_db.id if use_real_id else ulid.ULID(),
+        None,
+        'random_string',
+    )
+
+    assert org is None
