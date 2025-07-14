5ee0031dd1d8a75c2c7e41716c7ae6b467cba0c9
Aleksei Khatkevich <qwerty12345@disroot.org>
2025-07-14 14:24:38 +0300

    237

--- common/resources/database/ravendb/store.py
+++ common/resources/database/ravendb/store.py
@@ -1,24 +1,30 @@
+import contextlib
+
 from ravendb import DocumentStore
 
 from common import settings
 
 
-class DocumentStoreHolder:
-    _store: DocumentStore = None
+class RavenDBDocumentStore:
+    def __init__(self,
+                 url: str = settings.RAVEN_DB_SERVER_URL.unicode_string(),
+                 db_name: str = settings.RAVEN_DB_DATABASE_NAME,
+                 ) -> None:
+        self.url = url
+        self.db_name = db_name
+        self._store = None
 
-    @staticmethod
-    def _create_document_store() -> DocumentStore:
-        server_url = settings.RAVEN_DB_SERVER_URL.unicode_string()
-        database_name = settings.RAVEN_DB_DATABASE_NAME
+    @property
+    def store(self) -> DocumentStore:
+        if self._store is None:
+            store = DocumentStore(self.url, self.db_name)
+            store.initialize()
+            self._store = store
+        return self._store
 
-        document_store = DocumentStore([server_url], database_name)
+    @property
+    @contextlib.contextmanager
+    def session(self):
+        with self.store.open_session() as session:
+            yield session
 
-        document_store.initialize()
-        return document_store
-
-    @classmethod
-    def store(cls) -> DocumentStore:
-        if cls._store is None:
-            cls._store = cls._create_document_store()
-
-        return cls._store
