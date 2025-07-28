55031ee70896083c5d0cb2ff10e5f6343b7df80d
Aleksei Khatkevich <qwerty12345@disroot.org>
2025-07-28 13:22:48 +0300

    276

--- center/serializers/__init__.py
+++ center/serializers/__init__.py
@@ -3,3 +3,5 @@ from .organization import *
 from .operative_data import *
 from .header import *
 from .organization_stock import *
+from .current_info import *
+
---
+++ center/serializers/current_info.py
@@ -0,0 +1,54 @@
+import datetime
+from typing import Annotated
+
+import ulid
+from pydantic import BaseModel, Field
+
+__all__ = (
+    'OrganizationCurrentInfoOut',
+)
+
+from center.enums import Period
+
+
+class ProductionUnitCurrentInfo(BaseModel):
+    id: Annotated[
+        ulid.ULID,
+        Field(description='Id продукта.'),
+    ]
+    plan_value: Annotated[
+        float,
+        Field(description='Производственный план на сегодня.'),
+    ]
+    fact_value: Annotated[
+        float,
+        Field(description='Факт на сегодня.'),
+    ] = 0
+    instant_performance_value: Annotated[
+        float,
+        Field(description='Мгновенная текущая производительность',)
+    ]
+    instant_performance_period: Annotated[
+        Period,
+        Field(description='За какой период...', )
+    ] = Period.HOUR
+    suspended_now: Annotated[
+        bool,
+        Field('Остановлено ли производство данной продукции сейчас.',)
+    ] = False
+
+
+class OrganizationCurrentInfoOut(BaseModel):
+    id: Annotated[
+        ulid.ULID,
+        Field(description='Id компании.'),
+    ]
+    dt: Annotated[
+        datetime.datetime,
+        Field(default_factory=lambda: datetime.datetime.now(tz=datetime.UTC), description='Время состояния.'),
+    ]
+    current_data: Annotated[
+        list[ProductionUnitCurrentInfo],
+        Field(description='Производственные данные на конкретные продукты.', default_factory=list)
+    ]
+
