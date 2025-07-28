0858d9df21d53e35c937054337e37cd6ac9f159d
Aleksei Khatkevich <qwerty12345@disroot.org>
2025-07-28 17:26:39 +0300

    279 tmp

--- center/faststream/files.py
+++ center/faststream/files.py
@@ -1,3 +1,6 @@
+import tempfile
+
+import asyncssh
 import logfire
 from faststream.nats import NatsRouter, ObjWatch
 from faststream.nats.annotations import ObjectStorage
@@ -20,4 +23,16 @@ async def handler(
 ) -> None:
     file = await storage.get(filename)
     logfire.info('Got a file', data=file.data)
-    await storage.delete(filename)
+
+    # todo  aio version
+    with tempfile.NamedTemporaryFile(delete=True, delete_on_close=False) as temp_file:
+        temp_file.write(file.data)
+        temp_file.flush()
+
+        async with asyncssh.connect('localhost', username='sftpuser', password='1q2w3e') as conn:
+            async with conn.start_sftp_client() as sftp:
+                await sftp.put(temp_file.name, remotepath=file.info.name)
+
+        # await storage.delete(filename)
+
+
